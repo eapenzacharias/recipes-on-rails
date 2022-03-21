4c46e51735a27e6b486164bfdd5a01538bab3abb
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-03-21 15:36:52 -0500

    Generate recipe model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Food < ApplicationRecord
-    belongs_to :user
-    validates :name, presence: true
-    validates :measurement_unit, presence: true
-    validates :price, presence: true, numericality: {  greater_than_or_equal_to: 0 }
+  belongs_to :user
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    validates :name, presence: true
-    has_many :foods, dependent: :destroy
+  validates :name, presence: true
+  has_many :foods, dependent: :destroy
+  has_many :recipes, dependent: :destroy
 end
