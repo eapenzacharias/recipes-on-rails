ddd79bc0723f9813b5d1c380ad16ce02f4a090a2
eapenzacharias <eapenzac@gmail.com>
2022-03-22 19:06:13 +0100

    Updated controller for Recipe#index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,5 +2,8 @@
 
 # RecipesController
 class RecipesController < ApplicationController
-  def index; end
+  def index
+    @current_user = current_user
+    @recipes = @current_user.recipes
+  end
 end
