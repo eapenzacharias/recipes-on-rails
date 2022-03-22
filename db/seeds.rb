2bcf363e19acf8d943b4e9dd64e5b7424c2d4906
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-03-22 15:30:51 -0500

    Add new inventories to `seeds.rb`

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,7 @@
 user_1 = User.create(name: 'User 1', email: 'user1@test.com', password: '1234567', password_confirmation: '1234567')
 user_2 = User.create(name: 'User 2', email: 'user2@test.com', password: '1234567', password_confirmation: '1234567')
 
-food_1 = Food.create(name: 'Apple', measurement_unit: 'grams', price: '5', user: user_1)
-food_2 = Food.create(name: 'Pineapple', measurement_unit: 'grams', price: '1', user: user_1)
-food_3 = Food.create(name: 'Chicken breasts', measurement_unit: 'units', price: '2', user: user_1)
-# Receipe Seeds
+# Recipe Seeds
 
 recipe_1 = Recipe.create(name: 'Recipe 1', preparation_time: 45, cooking_time: 30, description: 'This is a description', public: true, user: user_1)
 recipe_2 = Recipe.create(name: 'Recipe 2', preparation_time: 85, cooking_time: 60, description: 'This is a description', public: true, user: user_1)
@@ -21,3 +18,13 @@ recipe_3 = Recipe.create(name: 'Recipe 3', preparation_time: 15, cooking_time: 1
 recipe_4 = Recipe.create(name: 'Recipe 4', preparation_time: 95, cooking_time: 65, description: 'This is a description', public: true, user: user_2)
 recipe_5 = Recipe.create(name: 'Recipe 5', preparation_time: 75, cooking_time: 60, description: 'This is a description', public: true, user: user_2)
 recipe_6 = Recipe.create(name: 'Recipe 6', preparation_time: 60, cooking_time: 35, description: 'This is a description', public: true, user: user_1)
+
+# Inventories
+Inventory.create(name: 'Inventory 1', user_id: 1)
+Inventory.create(name: 'Inventory 2', user_id: 1)
+Inventory.create(name: 'Inventory 3', user_id: 1)
+
+# Foods
+food_1 = Food.create(name: 'Apple', measurement_unit: 'grams', price: '5', user: user_1)
+food_2 = Food.create(name: 'Pineapple', measurement_unit: 'grams', price: '1', user: user_1)
+food_3 = Food.create(name: 'Chicken breasts', measurement_unit: 'units', price: '2', user: user_1)
