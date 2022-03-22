d0d6c2b0f8840d457c2c531e9e0d6a69b9b795b7
eapenzacharias <eapenzac@gmail.com>
2022-03-22 23:18:01 +0100

    Removed unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :foods, except: [:update]
   get 'inventories/index'
-  get 'recipes/index'
   resources :recipes, only: [:index, :show, :destroy]
   # Defines the root path route ("/")
   root "recipes#index"
