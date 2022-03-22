e9065005012a83b53f414d4f05ba3a4bb6bc177f
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-03-22 13:07:52 -0500

    Add `inventories#show` route

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,15 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+<<<<<<< HEAD
   resources :foods, except: [:update]
   get 'inventories/index'
   resources :recipes, only: [:index, :show, :destroy]
+=======
+  get 'foods/index'
+  resources :inventories, only: [:index, :show]
+  get 'recipes/index'
+>>>>>>> 0153214 (Add `inventories#show` route)
   # Defines the root path route ("/")
   root "recipes#index"
 end
