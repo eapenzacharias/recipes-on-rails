dbfc984b484e5fa6bff4dd1c52359df813054cbc
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-22 16:36:35 -0400

    Implement food create method

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -13,6 +13,19 @@ class FoodsController < ApplicationController
   end
 
   def create
+    @user = current_user
+    @food = Food.new(food_params)
+    @food.user = @user
+    respond_to do |format|
+      if @food.save
+
+        format.html { redirect_to foods_path, flash: { success: 'Food created successfully.' } }
+        format.json { render :index, status: :created, location: @food }
+      else
+        format.html { render :new, flash: { danger: @food.errors.messages } }
+        format.json { render json: @food.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
@@ -23,5 +36,11 @@ class FoodsController < ApplicationController
     @food.destroy
     flash.now[:success] = 'Food deleted successfully.'
     redirect_to foods_path
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
   end
 end
