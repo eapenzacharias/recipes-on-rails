279079a48d34b053c10e42507516452d4a38dd3a
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-03-22 14:40:06 -0500

    Add option to add a new inventory

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -14,4 +14,25 @@ class InventoriesController < ApplicationController
     @inventory.destroy
     redirect_to inventories_path
   end
+
+  def new
+    @inventory = Inventory.new
+  end
+
+  def create
+    @user = current_user
+    inventory = Inventory.new(inventory_params.merge(user_id: @user.id))
+    if inventory.save
+      redirect_to inventories_path
+    else
+      flash[:alert] = 'Somthing went wrong.'
+      render :new
+    end
+  end
+
+  private
+
+  def inventory_params
+    params.require(:inventory).permit(:name)
+  end
 end
