c63ea93b0f8a8bf964d384d7cd567bfaad515de9
eapenzacharias <eapenzac@gmail.com>
2022-03-22 22:39:07 +0100

    Updated Recipe controller and routes for delete

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'foods/index'
   get 'inventories/index'
   get 'recipes/index'
+  resources :recipes, only: [:index, :show, :destroy]
   # Defines the root path route ("/")
   root "recipes#index"
 end
