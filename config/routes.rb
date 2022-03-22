ae3341ed27f9847dd52101d38a820e546d6d12cd
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-21 21:20:55 -0400

    Add food route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :foods, except: [:update]
   # Defines the root path route ("/")
   # root "articles#index"
 end
