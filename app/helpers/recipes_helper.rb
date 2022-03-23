02f8d4bb8b49998a90a33c80f693909ca39bbe36
eapenzacharias <eapenzac@gmail.com>
2022-03-23 23:50:35 +0100

    Public recipe views updated & added helper methods

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -2,4 +2,14 @@
 
 # RecipesHelper
 module RecipesHelper
+  def count_items(obj)
+    obj.count
+  end
+
+  def total_price(recipe)
+    sum = 0
+    recipe.recipe_foods&.each { |i| sum += i.quantity * i.food.price}
+    sum
+  end
+
 end
