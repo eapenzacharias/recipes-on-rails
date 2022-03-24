0538c45064aad9ddfdb521e6a666729f918013bd
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-24 15:09:20 -0400

    Add shopping list stats and prepare controller

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,4 +1,7 @@
 class ShoppingListController < ApplicationController
   def index
+    @recipe = Recipe.find_by_id(params[:recipe_id])
+    @inventory = Inventory.find_by_id(params[:inventory_id])
+    @shopping_list = []
   end
 end
