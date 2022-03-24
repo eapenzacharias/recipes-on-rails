c3fc7871a4bae1e3bf313ce06521a775f73eae9d
eapenzacharias <eapenzac@gmail.com>
2022-03-24 17:42:07 +0100

    Fixed dependent ActiveRecord error

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,7 +2,7 @@
 
 class Inventory < ApplicationRecord
   belongs_to :user
-  has_many :inventory_foods
+  has_many :inventory_foods, dependent: :destroy
   has_many :foods, through: :inventory_foods
   validates :name, presence: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,6 @@ class Recipe < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
   validates :public, inclusion: { in: [true, false] }
-  has_many :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
 end
