1645e163181454781c5f133276d6219558edede0
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-24 13:23:50 -0400

    Generate shopping list controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -11,13 +12,14 @@ Rails.application.routes.draw do
     post 'foods', to: 'inventory_foods#create'
     delete 'foods/:id', to: 'inventory_foods#destroy', as: 'food'
   end
-
-  resources :recipes, only: [:index, :show, :new, :create, :destroy, :put ] do
+  resources :recipes, only: [:index, :show, :destroy, :put ] do
     resources :recipe_foods, path: 'food', only: [:destroy, :new, :create]
   end
+
   match 'recipes/:recipe_id' => 'recipes#toogle_public', as: :toogle_public, via: :patch
   match 'public_recipes' => 'recipes#public_recipes', as: :public_recipes, via: :get
 
+  get 'shopping_list/index'
   # Defines the root path route ("/")
   root 'recipes#public_recipes'
 end
